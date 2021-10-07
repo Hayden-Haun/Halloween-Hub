f8c763972585f8e49a19e3260a4a454828083dd1
Abby Castelow <abby.castelow@gmail.com>
2021-10-07 15:42:58 -0600

    updated countdown timer

--- client/src/components/BannerHome/BannerHome.js
+++ client/src/components/BannerHome/BannerHome.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
-import Countdown from '../Countdown/Coutdown';
+import Countdown from '../Countdown/Countdown';
 
 function BannerHome() {
   const hoursMinSecs = {hours:1, minutes: 20, seconds: 40}
   return (
     <>
       <div className="bannerBackground my-0 py-0">
-      <Countdown hoursMinSecs={hoursMinSecs} />
+      <Countdown/>
       </div>
       <div className="columns is-gapless my-0 py-0">
         <div className="column boxGrid has-background-dark">
---
+++ client/src/components/Countdown/Countdown.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+
+function Countdown() {
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    const difference = +new Date(`${year}-10-31`) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [year] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+  return (
+    <div class = "countdown">
+      <h1>Countdown to Halloween {year}</h1>
+      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+    </div>
+  );
+}
+
+export default Countdown;
+
+
