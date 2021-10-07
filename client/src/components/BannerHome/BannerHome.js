51a7bd52e26f12263b532218fc872b5092573c7e
Hayden-Haun <haydenhaun@gmail.com>
2021-10-07 10:33:14 -0600

    modified top level structure so that home page only displays banner, we have pages for all products and products by category

--- client/src/components/BannerHome/BannerHome.js
+++ client/src/components/BannerHome/BannerHome.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function BannerHome() {
@@ -6,7 +7,11 @@ function BannerHome() {
     <>
       <div className="bannerBackground my-0 py-0"></div>
       <div className="columns is-gapless my-0 py-0">
-        <div className="column boxGrid has-background-dark">Box1</div>
+        <div className="column boxGrid has-background-dark">
+          <button className="is-light button">
+            <Link to="/products">See all products</Link>
+          </button>
+        </div>
         <div className="column boxGrid boxImage1">Box2</div>
       </div>
       <div className="columns is-gapless my-0 py-0">
