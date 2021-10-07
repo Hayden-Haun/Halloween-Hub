bd56aa6bcf9154ba99d10700e6db7e84a3c34c7e
Nicholas Wojtalewicz <nicholaswojt@outlook.com>
2021-10-07 13:56:18 -0600

    made styling changes for the login and sign up pages, functionality is still operational

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
 
 function Signup(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
@@ -31,53 +31,69 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
+    <div className="container my-1 is-flex is-justify-content-center is-align-items-center is-flex-direction-column py-5">
+      <Link to="/login" className="is-size-4">
+        ← Go to Login
+      </Link>
 
-      <h2>Signup</h2>
+      <h2 className="is-size-2">Signup</h2>
       <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
+        <div className="field">
+          <label htmlFor="firstName" className="label">
+            First Name:
+          </label>
           <input
             placeholder="First"
             name="firstName"
             type="firstName"
             id="firstName"
             onChange={handleChange}
+            className="input"
           />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
+        <div className="field">
+          <label htmlFor="lastName" className="label">
+            Last Name:
+          </label>
           <input
             placeholder="Last"
             name="lastName"
             type="lastName"
             id="lastName"
             onChange={handleChange}
+            className="input"
           />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
+        <div className="field">
+          <label htmlFor="email" className="label">
+            Email:
+          </label>
           <input
             placeholder="youremail@test.com"
             name="email"
             type="email"
             id="email"
             onChange={handleChange}
+            className="input"
           />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
+        <div className="field">
+          <label htmlFor="pwd" className="label">
+            Password:
+          </label>
           <input
             placeholder="******"
             name="password"
             type="password"
             id="pwd"
             onChange={handleChange}
+            className="input"
           />
         </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
+        <div className="field is-flex is-justify-content-center is-align-items-center">
+          <button className="button is-success" type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </div>
