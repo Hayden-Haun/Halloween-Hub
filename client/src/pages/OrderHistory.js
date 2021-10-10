03de0be8a305daee16441ee300dbad373c28d13e
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-10-10 15:56:53 -0600

    Updated Order History styling

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { useQuery } from "@apollo/client";
+import { QUERY_USER } from "../utils/queries";
 
 function OrderHistory() {
   const { data } = useQuery(QUERY_USER);
@@ -14,7 +14,7 @@ function OrderHistory() {
 
   return (
     <>
-      <div className="container my-1">
+      <div style={{ height: "100%" }} className="container pb-4">
         <Link to="/">← Back to Products</Link>
 
         {user ? (
@@ -24,18 +24,20 @@ function OrderHistory() {
             </h2>
             {user.orders.map((order) => (
               <div key={order._id} className="my-2">
-                <h3>
-                  {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
-                </h3>
                 <div className="flex-row">
                   {order.products.map(({ _id, image, name, price }, index) => (
-                    <div key={index} className="card px-1 py-1">
-                      <Link to={`/products/${_id}`}>
-                        <img alt={name} src={`/images/${image}`} />
-                        <p>{name}</p>
-                      </Link>
-                      <div>
-                        <span>${price}</span>
+                    <div className="card mt-5 column has-background-white">
+                      <h3>Puchased on {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}</h3>
+                      <div key={index} className="card-image my-4">
+                        <figure>
+                          <Link to={`/products/${_id}`}>
+                            <img alt={name} src={`/images/${image}`} />
+                            <div>
+                              <p className="title is-4 has-text-centered">{name}</p>
+                              <p className="subtitle is-6 has-text-centered">You last paid ${price} for this item</p>
+                            </div>
+                          </Link>
+                        </figure>
                       </div>
                     </div>
                   ))}
