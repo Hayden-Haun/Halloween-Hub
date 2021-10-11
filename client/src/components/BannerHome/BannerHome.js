dfa28fac62f0ea0e0fc065d34f55a9183d0cdc36
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-10-10 20:30:16 -0600

    Made homepage elements responsive under 768 screen sizes

--- client/src/components/BannerHome/BannerHome.js
+++ client/src/components/BannerHome/BannerHome.js
@@ -7,24 +7,22 @@ function BannerHome() {
   const hoursMinSecs = { hours: 1, minutes: 20, seconds: 40 };
   return (
     <>
-      <div className="bannerBackground my-0 py-0">
+      <div className="is-flex is-justify-content-center bannerBackground my-0 py-0">
         <Countdown />
       </div>
       <div className="columns is-gapless my-0 py-0">
-        <div className="is-flex-direction-column is-flex is-justify-content-center boxGrid has-background-dark">
-          <p className="is-align-self-center ad-text">Content</p>
-          <button className="is-align-self-center is-light button">
-            <Link to="/products">See all products</Link>
+        <div className="is-flex-direction-column is-flex is-justify-content-center boxGrid has-background-light">
+          <button className="is-align-self-center is-primary button">
+            <Link to="/products">Shop now</Link>
           </button>
         </div>
         <div className="column boxGrid boxImage1"></div>
       </div>
       <div className="columns is-gapless my-0 py-0">
         <div className="column boxGrid boxImage2"></div>
-        <div className="is-flex-direction-column is-flex is-justify-content-center boxGrid has-background-dark">
-          <p className="is-align-self-center ad-text">Content</p>
-          <button className="is-align-self-center is-light button">
-            <Link to="/products">See all products</Link>
+        <div className="is-flex-direction-column is-flex is-justify-content-center boxGrid has-background-light">
+          <button className="is-align-self-center is-primary button">
+            <Link to="/products">Shop now</Link>
           </button>
         </div>
       </div>
